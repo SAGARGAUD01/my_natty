820a89df825464300ea29893181b838ed2c73069
sagar gaud <81501729+SAGARGAUD01@users.noreply.github.com>
2024-07-07 13:46:11 +0300

    files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Sagar_Gaud-portfolio
+# Sagar's Portfolio
+My personal portfolio ,This is Sagar Gaud currently engaged with Google Summer of Code(GSoC24) at @Jenkins (Implementing UI for Jenkins Infra Statistics)
+Link- 
---
+++ js/style-switcher.js
@@ -0,0 +1,57 @@
+const styleSwitcherToggler=document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggler.addEventListener("click" , () =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+
+window.addEventListener("scroll",() =>{
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        (document.querySelector(".style-switcher").classList.remove("open"));
+    }
+})
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+console.log(alternateStyles);
+function setActiveStyle(color){
+    /*  */
+
+    localStorage.setItem("color",color);
+    changeColor();  
+}
+
+function changeColor(){
+    alternateStyles.forEach((style) => {
+        if(localStorage.getItem("color")===style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }else{
+            style.setAttribute("disabled" , "true")
+        }
+        
+    });
+}
+
+if(localStorage.getItem("color") !==null){
+    changeColor();
+}
+
+//my fav part ji
+
+const dayNight=document.querySelector(".day-night");
+dayNight.addEventListener("click",()=>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark")
+
+})
+
+
+ //my fav part ji
+window.addEventListener("load",()=>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+
+})
